9941068a8739e6ec3d99528a057f5138b4814705
YB0504 <yeo9411@naver.com>
2025-04-24 23:57:52 +0900

    Again Java Day 13 PM

---
+++ src/access/exercise/Item.java
@@ -0,0 +1,21 @@
+package access.exercise;
+
+public class Item {
+    String name;
+    int price;
+    int quantity;
+
+    public Item(String name, int price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int totalPrice() {
+        return price * quantity;
+    }
+}
