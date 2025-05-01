44ebfa0db5fa34c7a5d816c55d256ba09afb60dc
INBEOM YEO <yeo9411@naver.com>
2025-05-02 08:46:40 +0900

    Again Java Day 19

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,5 +1,7 @@
 package static2;
 
+import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         // 정적 메소드가 인스턴스의 기능을 사용할 수 없는 이유
@@ -17,5 +19,11 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall();
+
+        // static import 적용
+        // 정적 메소드를 사용할 때 특정 메소드를 자주 호출해야한다면 static import를 고려하자
+        // static import를 적용하면 클래스명을 생략하고 메소드를 호출할 수 있다.
+        System.out.println("static import 적용 정적 호출");
+        staticCall();
     }
 }
